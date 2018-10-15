4a34f3cf89b6ceabf39a29f3d534e5d291295b9a
sevcik <pavel.sevcik@etnetera.cz>
2018-10-15 15:58:33 +0200

    Prepare App for next workshop iteration

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,11 @@
+const createDataElement = () => {
+    const dataElement = document.createElement('div');
+
+    dataElement.id = 'data';
+
+    return dataElement;
+}
+
 export default class App {
     constructor() {
         this.data = {
@@ -5,18 +13,61 @@ export default class App {
             people: {},
             ships: {},
         }
+        this.dataContainer = document.getElementById('data-container');
     }
 
     init() {
         const filmsBtn = document.getElementById('load-films');
-        // TODO: implement this method
+
+        filmsBtn.onclick = () => {
+            this.renderFilms();
+        }
     }
 
     renderLoading() {
-        // TODO: implement this method
+        const dataElement = createDataElement();
+        const loadingElement = document.createElement('div')
+
+        loadingElement.className = 'loading'
+
+        dataElement.appendChild(loadingElement);
+
+        this.dataContainer.replaceChild(dataElement, document.getElementById('data'))
+    }
+
+    renderError(error) {
+        const dataElement = createDataElement();
+
+        dataElement.innerText = `Nastala chyba při zpracování requestu. ${error}`
+
+        this.dataContainer.replaceChild(dataElement, document.getElementById('data'))
     }
 
     renderFilms() {
-        // TODO: implement this method
+        const dataElement = createDataElement();
+
+        this.renderLoading();
+        fetch('https://swapi.co/api/films/').
+            then((response) => response.json()).
+            then((data) => {
+                data.results.forEach((film) => {
+                    const filmElement = document.createElement('div');
+                    const filmTitle = document.createElement('h3');
+                    const filmDescription = document.createElement('p');
+
+                    filmTitle.innerText = film.title;
+                    filmDescription.innerText = film.opening_crawl;
+
+                    filmElement.appendChild(filmTitle);
+                    filmElement.appendChild(filmDescription);
+
+                    dataElement.appendChild(filmElement)
+                });
+
+                this.dataContainer.replaceChild(dataElement, document.getElementById('data'))
+            }).
+            catch((error) => {
+                this.renderError(error);
+            })
     }
 }
