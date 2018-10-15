913cc78a37d4d6b2956f76adaa93236d2f239600
sevcik <pavel.sevcik@etnetera.cz>
2018-10-15 16:23:45 +0200

    Fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -99,7 +99,6 @@ module.exports = {
         "max-lines-per-function": "error",
         "max-nested-callbacks": "error",
         "max-params": "error",
-        "max-statements": "error",
         "max-statements-per-line": "error",
         "multiline-comment-style": "error",
         "multiline-ternary": "error",
