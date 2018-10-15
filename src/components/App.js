c59a2e01a3c0a5e2a4f259b1fe28bbe3c8453d76
sevcik <pavel.sevcik@etnetera.cz>
2018-10-15 16:26:49 +0200

    ships -> starship as is in API

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ export default class App {
         this.data = {
             films: {},
             people: {},
-            ships: {},
+            starships: {},
         }
         this.dataContainer = document.getElementById('data-container');
     }
